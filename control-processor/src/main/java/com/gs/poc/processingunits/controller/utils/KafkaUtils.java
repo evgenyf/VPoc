5b34fd1ac467bcb2ee980153b6a05c83957046d3
Evgeny Fisher <evgenyf@gmail.com>
2020-09-14 18:46:43 +0300

    increase max number of polling events, logs changed

--- control-processor/src/main/java/com/gs/poc/processingunits/controller/utils/KafkaUtils.java
+++ control-processor/src/main/java/com/gs/poc/processingunits/controller/utils/KafkaUtils.java
@@ -15,14 +15,14 @@ import java.util.stream.Collectors;
 
 public class KafkaUtils {
 
-    public static Consumer createConsumer( Properties kafkaProps, Class valueDeserializerClass, String topic ){
-        KafkaConsumer kafkaConsumer = new KafkaConsumer(KafkaUtils.initConsumerProperties(kafkaProps, valueDeserializerClass ));
+    public static <K, V> Consumer<K, V> createConsumer( Properties kafkaProps, Class valueDeserializerClass, String topic ){
+        KafkaConsumer<K, V> kafkaConsumer = new KafkaConsumer<>(KafkaUtils.initConsumerProperties(kafkaProps, valueDeserializerClass ));
         kafkaConsumer.assign(KafkaUtils.initTopicPartitions( kafkaConsumer, topic ));
         return kafkaConsumer;
     }
 
-    public static Producer createProducer(Properties kafkaProps, Class valueSerializerClass){
-        return new KafkaProducer( initKafkaProducerProperties( kafkaProps, valueSerializerClass ) );
+    public static <K, V> Producer<K, V> createProducer(Properties kafkaProps, Class valueSerializerClass){
+        return new KafkaProducer<>( initKafkaProducerProperties( kafkaProps, valueSerializerClass ) );
     }
 
     private static Set<TopicPartition> initTopicPartitions(Consumer consumer, String topic ){
@@ -49,6 +49,9 @@ public class KafkaUtils {
         props.putIfAbsent(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,"org.apache.kafka.common.serialization.StringDeserializer");
         props.putIfAbsent(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, valueDeserializerClass.getName());
         props.putIfAbsent(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"earliest");
+        props.putIfAbsent(ConsumerConfig.MAX_POLL_RECORDS_CONFIG,10_000);
+        //props.putIfAbsent(ConsumerConfig.FETCH_MAX_BYTES_CONFIG,500*1024*1024);
+
         return com.gs.poc.kafka.utils.Utils.toProperties(props);
     }
 
